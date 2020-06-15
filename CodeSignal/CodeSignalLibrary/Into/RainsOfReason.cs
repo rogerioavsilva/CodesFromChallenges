58a042f48032032069e47dc2134bf39d0c5d24c4
Rogério Aluisio Vicente da Silva <rogerioavsilva@gmail.com>
2020-06-15 16:15:22 -0300

    Init project

---
+++ CodeSignal/CodeSignalLibrary/Into/RainsOfReason.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeSignalLibrary
+{
+    public class RainsOfReason
+    {
+        public static bool EvenDigitsOnly(in int input)
+        {
+            var numbers = input.ToString();
+
+            foreach (var num in numbers)
+                if ((int)num % 2 != 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ CodeSignal/CodeSignalXUnitTest/Intro/RainsOfReasonTest.cs
@@ -0,0 +1,27 @@
+﻿using CodeSignalLibrary;
+using Xunit;
+
+namespace CodeSignalXUnitTest.Intro
+{
+    public class RainsOfReasonTest
+    {
+        [Fact]
+        public void EvenDigitsOnlyTest1()
+        {
+            //Arrange
+            var input = 248622;
+            //Act
+            bool isEven = RainsOfReason.EvenDigitsOnly(input);
+            //Assert
+            Assert.True(isEven);
+
+        }
+
+        [Theory]
+        [InlineData(true, 248622)]
+        [InlineData(false, 642386)]
+        public void EvenDigitsOnlyTest2(bool expected, int input) =>
+            Assert.Equal(expected, RainsOfReason.EvenDigitsOnly(input));
+
+    }
+}
